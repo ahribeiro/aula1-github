30e4363111118ab2def90cecb3aee60346de99b2
Anderson Ribeiro <anderson.hhenriqque@gmail.com>
2021-08-22 14:25:19 -0300

    Acrescentada mensagem de bom dia

--- src/main/java/com/mycompany/course/Program.java
+++ src/main/java/com/mycompany/course/Program.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.course;
 
-/**
- *
- * @author Anderson
- */
 public class Program {
+    public static void main(String[] args) {
+        System.out.println("Bom dia");
+    }
     
 }
