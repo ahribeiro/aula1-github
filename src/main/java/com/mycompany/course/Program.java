44dcd875c90a1294a7a6ee86bc202a5253defa6d
Anderson Ribeiro <anderson.hhenriqque@gmail.com>
2021-08-22 20:01:55 -0300

    Acrescentada a mensagem de boa tarde

--- src/main/java/com/mycompany/course/Program.java
+++ src/main/java/com/mycompany/course/Program.java
@@ -3,6 +3,7 @@ package com.mycompany.course;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Bom dia");
+        System.out.println("Boa tarde");
     }
     
 }
